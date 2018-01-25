b4ac31387ab5ef7daeca05f77d99cdca05ef345c
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-01-25 00:46:11 -0200

    Fixing 503 heroku error step 2

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose; // This line is the equivalent of: const Schema = mongoose.Schema;
+
+// Schema is used to create a frame for the objects in Mongo.
+// Fixed name
+const userSchema = new Schema({
+	googleID: String,
+	credits: { type: Number, default: 0 }
+});
+
+mongoose.model('users', userSchema); // Creates the collection if it doesn't exist.
